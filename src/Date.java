dcbc4c1348ceac0139e5d5786ff0cbb52f868253
BuildTools <jmeredit@ashland.edu>
2018-02-09 10:45:50 -0500

    Update
    Update

--- src/Date.java
+++ src/Date.java
@@ -11,12 +11,24 @@ public class Date {
 			String input;
 			int month,
 				day,
-				year;
+				year,
+				validDay;
+			
+
+			input = JOptionPane.showInputDialog("Enter the Year");
+			year = Integer.parseInt(input);
+			
+			if(year<=10000) {
+				
+			}
+			
 			
 			input = JOptionPane.showInputDialog("Enter the Month");
 			month = Integer.parseInt(input);
 			
-			JOptionPane.showMessageDialog(null, month);
+			if(!(1>=month || month<=12)) {
+					JOptionPane.showMessageDialog(null, "Not valid");
+			}
 			
 			input = JOptionPane.showInputDialog("Enter the Day");
 			day = Integer.parseInt(input);
