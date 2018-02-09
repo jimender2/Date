b5f78d36e21876ee239ee6275bc21dc17b9d3ccb
BuildTools <jmeredit@ashland.edu>
2018-02-09 10:23:19 -0500

    Get Input
    Get input from users

--- src/Date.java
+++ src/Date.java
@@ -1,11 +1,28 @@
+import javax.swing.JOptionPane;
+
 /*
  * Jonathan Meredith
  */
-import javax.swing.JOptionPane;
 
 public class Date {
 
 	public static void main(String[] args) {
+			
+			String input;
+			int month,
+				day,
+				year;
+			
+			input = JOptionPane.showInputDialog("Enter the Month");
+			month = Integer.parseInt(input);
+			
+			JOptionPane.showMessageDialog(null, month);
+			
+			input = JOptionPane.showInputDialog("Enter the Day");
+			day = Integer.parseInt(input);
+
+			input = JOptionPane.showInputDialog("Enter the Year");
+			month = Integer.parseInt(input);
 
 	}
 
